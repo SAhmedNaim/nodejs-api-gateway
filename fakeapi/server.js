09835306048d488cb24a0fdc63ef593f5f9993d5
unknown <naim.ahmed035@gmail.com>
2021-10-30 00:16:37 +0600

    API Registration while running new API Server

--- fakeapi/server.js
+++ fakeapi/server.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
+const axios = require('axios')
 
 const PORT = 3001
+const HOST = "http://localhost"
 
 app.use(express.json())
 app.get('/fakeapi', (req, res, next) => {
@@ -17,5 +19,20 @@ app.post('/vote', (req, res, next) => {
 })
 
 app.listen(PORT, () => {
+    // Register this API to the API Gateway
+    axios({
+        method: 'POST',
+        url: 'http://localhost:3000/register/',
+        headers: {'Content-Type': 'application/json'},
+        data: {
+            apiName: "registrytest",
+            host: HOST,
+            port: PORT,
+            url: `${HOST}:${PORT}`
+        }
+    }).then((response) => {
+        console.log(response.data)
+    })
+
     console.log(`Hello from fake api server from the PORT ${PORT}`);
 })
